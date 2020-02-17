73c442e879b02e5da8f1c9658b057eca3dd6a1cb
Mathias <mathias.skallerud@gmail.com>
2020-02-17 14:01:56 +0100

    Add function makeNewDeck() in Deck

--- src/main/java/inf112/skeleton/app/cards/Deck.java
+++ src/main/java/inf112/skeleton/app/cards/Deck.java
@@ -1,27 +1,94 @@
 package inf112.skeleton.app.cards;
 
+import inf112.skeleton.app.enums.Rotate;
+
 import java.util.Collections;
 import java.util.Stack;
 
 public class Deck {
     private Stack<ProgramCard> deck;
 
-    public Deck(){
-        //TODO: The deck can be made in the constructor or the constructor can call a func (?). Need to see the
-        // rule-book for have many of the different cards there are in the deck.
+    public Deck() {
+
+        makeNewDeck();
+        shuffleDeck();
     }
 
-    /** @return the size of the deck */
-    public int deckSize(){
+    /**
+     * Makes all the programCards and puts them on the stack
+     */
+    public void makeNewDeck() {
+        deck = new Stack<>();
+        int priority = 0;
+        // Making RotateR (18) and RotateL (18) cards
+        for (int i = 0; i < 36; i++) {
+            ProgramCard card;
+            priority += 10;
+            if (i % 2 == 0) {
+                card = new ProgramCard(priority, 0, Rotate.LEFT, "Rotate Left");
+            } else {
+                card = new ProgramCard(priority, 0, Rotate.RIGHT, "Rotate Right");
+            }
+            deck.push(card);
+        }
+        for (int i = 0; i < 6; i++) {
+            priority += 10;
+            ProgramCard card = new ProgramCard(priority, 0, Rotate.UTURN, "U-Turn");
+            deck.push(card);
+        }
+        // TODO: Finne en måte å koble den sammen med MOVE 2 og MOVE 3.
+
+        for (int i = 0; i < 18; i++) {
+            priority += 20;
+            ProgramCard card = new ProgramCard(priority, 1, Rotate.NONE, "Move 1");
+            deck.push(card);
+        }
+        // Making Move 2 (12) and Move 3 (6) and Move 1 (12) cards
+        boolean notGoodVariableName = true;
+        for (int i = 0; i < 30; i++) {
+            priority += 10;
+            ProgramCard card;
+
+            if (i % 3 == 0 && i < 18) {
+                card = new ProgramCard(priority, 3, Rotate.NONE, "Move 3");
+            } else {
+                if (notGoodVariableName) {
+                    card = new ProgramCard(priority, 2, Rotate.NONE, "Move 2");
+                    notGoodVariableName = false;
+                } else {
+                    card = new ProgramCard(priority, 1, Rotate.NONE, "Move 1");
+                    notGoodVariableName = true;
+                }
+            }
+            deck.push(card);
+        }
+        // Making the last 6 Move 1 cards
+        for (int i = 0; i < 6; i++) {
+            priority += 10;
+            ProgramCard card = new ProgramCard(priority, 1, Rotate.NONE, "Move 1");
+            deck.push(card);
+        }
+
+    }
+
+    /**
+     * @return the size of the deck
+     */
+    public int deckSize() {
         return deck.size();
     }
-    /** Shuffles the deck */
-    public void shuffleDeck(){
+
+    /**
+     * Shuffles the deck
+     */
+    public void shuffleDeck() {
         Collections.shuffle(deck);
     }
 
-    /** @return the next {@link ProgramCard} in the stack / deck */
-    public ProgramCard drawCard(){
+    /**
+     * @return the next {@link ProgramCard} in the stack / deck
+     */
+    public ProgramCard drawCard() {
         return deck.pop();
     }
 
--- src/main/java/inf112/skeleton/app/cards/ProgramCard.java
+++ src/main/java/inf112/skeleton/app/cards/ProgramCard.java
@@ -5,15 +5,16 @@ import inf112.skeleton.app.enums.Rotate;
 public class ProgramCard {
     private int priority;
     private int distance;
+    private String name;
 
     private Rotate rotate;
 
-    public ProgramCard(int priority, int distance, Rotate rotate){
+    public ProgramCard(int priority, int distance, Rotate rotate, String name){
         this.priority = priority;
         this.distance = distance;
         this.rotate = rotate;
+        this.name = name;
     }
-
 
     public int getPriority() {
         return priority;
