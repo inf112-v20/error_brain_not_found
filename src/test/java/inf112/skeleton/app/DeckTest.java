cf62dd5fed726fb6b02b508741a0be9f7a4a29fa
JennStro <jennys1994@gmail.com>
2020-02-17 19:54:01 +0100

    Test when first cards are drawed from deck

--- src/test/java/inf112/skeleton/app/DeckTest.java
+++ src/test/java/inf112/skeleton/app/DeckTest.java
@@ -1,6 +1,8 @@
 package inf112.skeleton.app;
 
 import inf112.skeleton.app.cards.Deck;
+import inf112.skeleton.app.cards.ProgramCard;
+import inf112.skeleton.app.enums.Rotate;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,6 +11,8 @@ import static org.junit.Assert.assertEquals;
 public class DeckTest {
 
     private Deck deck;
+    private final int NUMBER_OF_MOVE_ONE_CARDS = 6;
+    private final int NUMBER_OF_CARDS = 78;
 
     @Before
     public void setUp() {
@@ -16,8 +20,22 @@ public class DeckTest {
     }
 
     @Test
-    public void whenNewDeckIsMadeTheNumberOfProgramCardsInDeckIs78() {
-        assertEquals(78, deck.deckSize());
+    public void whenNewDeckIsMadeTheNumberOfProgramCardsInDeckIsNumberOfCards() {
+        assertEquals(NUMBER_OF_CARDS, deck.deckSize());
+    }
+
+    @Test
+    public void whenNewDeckIsMadeButNotShuffledFirstCardIsAMove1Card() {
+        deck.makeNewDeck();
+        assertEquals(1, deck.drawCard().getDistance());
+    }
+
+    @Test
+    public void whenDeckIsNotShuffledTheFirstCardsAreOfTypeMoveOneCard() {
+        deck.makeNewDeck();
+        for (int i = 0; i < NUMBER_OF_MOVE_ONE_CARDS; i++) {
+            assertEquals(1, deck.drawCard().getDistance());
+        }
     }
 
 
