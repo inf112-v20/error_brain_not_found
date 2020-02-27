d0c2482af7b47540a6266d5ae4808cf483501718
callmedeku <hamed.yad@hotmail.com>
2020-02-27 10:56:15 +0100

    Made a TileID enums(2)

--- src/main/java/inf112/skeleton/app/enums/TileID.java
+++ src/main/java/inf112/skeleton/app/enums/TileID.java
@@ -8,7 +8,41 @@ public enum TileID {
     STARTPOS5(129),
     STARTPOS6(130),
     STARTPOS7(131),
-    STARTPOS8(133);
+    STARTPOS8(133),
+
+    RIGHT_WALL(23),
+    DOWN_WALL(29),
+    LEFT_WALL(30),
+    UP_WALL(31),
+
+    RIGHT_LASER_WALL(46),
+    DOWN_LASER_WALL(37),
+    LEFT_LASER_WALL(38),
+    UP_LASER_WALL(45),
+
+
+
+    SOUTHEASTWALL(8),
+    NORTHEASTWALL(16),
+    NORTHWESTWALL(24),
+    SOUTHWESTWALL(32),
+
+
+    VERTICAL_LASER(47),
+    HORIZONTAL_LASER(39),
+
+
+
+
+
+
+    WRENCH(14),
+    DOUBLE_WRENCH(7);
+
+
+
+
+
 
     private int id;
 
