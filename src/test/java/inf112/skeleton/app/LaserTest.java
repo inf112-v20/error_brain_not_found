cd6f281d00165fec61eabf5b8b67cf6c910f83b7
JennStro <jennys1994@gmail.com>
2020-03-20 19:10:31 +0100

    When laser is activated it has lasers in startposition

--- src/test/java/inf112/skeleton/app/LaserTest.java
+++ src/test/java/inf112/skeleton/app/LaserTest.java
@@ -3,22 +3,25 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.enums.Direction;
 import inf112.skeleton.app.objects.Laser;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 public class LaserTest {
 
     private RallyGame game;
     private Board board;
-    private Laser laser;
     private ArrayList<Laser> lasers;
 
     @Before
@@ -29,8 +32,29 @@ public class LaserTest {
         this.game = new RallyGame();
         this.game.setupGame("assets/maps/Risky_Exchange.tmx");
         this.board = game.getBoard();
-        this.laser = new Laser(0,0, Direction.NORTH);
         this.lasers = board.lasers;
+    }
+
+    /**
+     *
+     * @param lasers
+     * @return random laser laser from list given
+     */
+    private Laser getRandomLaser(ArrayList<Laser> lasers) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(lasers.size());
+        return lasers.get(randomIndex);
+    }
+
+    /**
+     *
+     * @param position
+     * @return true if cell in this position from laserLayer is not null (has a laser on this cell).
+     */
+    private boolean hasLaser(Vector2 position) {
+        TiledMapTileLayer laserLayer = board.getLaserLayer();
+        TiledMapTileLayer.Cell cell = laserLayer.getCell((int) position.x, (int) position.y);
+        return cell != null;
     }
 
     @Test
@@ -38,5 +62,15 @@ public class LaserTest {
         assertFalse(lasers.isEmpty());
     }
 
+    @Test
+    public void lasersFiredHasLasersInStartPositionTest() {
+        for (int i = 0; i < 3; i++) {
+            Laser laser = getRandomLaser(lasers);
+            Vector2 laserPosition = laser.getStartPosition();
+            //TODO: Let RallyGame take in number of players as arg
+            laser.fire(game);
+            assertTrue(hasLaser(laserPosition));
+        }
+    }
 
 }
