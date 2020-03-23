13671de062243806a7a58a76ef9a2032eb042e3b
Mathias <mathias.skallerud@gmail.com>
2020-03-23 19:45:01 +0100

    Add belt constructor

--- src/main/java/inf112/skeleton/app/objects/Belt.java
+++ src/main/java/inf112/skeleton/app/objects/Belt.java
@@ -1,4 +1,37 @@
 package inf112.skeleton.app.objects;
 
+import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.app.enums.Direction;
+import inf112.skeleton.app.enums.Rotate;
+
 public class Belt {
+
+    private Vector2 position;
+
+    private Direction direction;
+    private int power;
+    private Rotate rotate;
+
+    public Belt(Direction direction, Rotate rotate, int power, Vector2 position){
+        this.direction = direction;
+        this.position = position;
+        this.power = power;
+        this.rotate = rotate;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public Rotate getRotate() {
+        return rotate;
+    }
 }
