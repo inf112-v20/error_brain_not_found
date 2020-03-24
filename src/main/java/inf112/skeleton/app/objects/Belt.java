bbb5cdc0b748ea40d9de970fb4d2b8e89e04774c
Mathias <mathias.skallerud@gmail.com>
2020-03-24 14:48:20 +0100

    Add javadoc to belt

--- src/main/java/inf112/skeleton/app/objects/Belt.java
+++ src/main/java/inf112/skeleton/app/objects/Belt.java
@@ -2,7 +2,17 @@ package inf112.skeleton.app.objects;
 
 import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.enums.Direction;
+import inf112.skeleton.app.enums.Rotate;
 
+/**
+ * <p>
+ *     A object to mimic a belt in the game.
+ * </p>
+ *
+ * <p>
+ * Since the belts can har two different {@link Rotate}, the {@link Direction} of the {@link inf112.skeleton.app.Player} should be set to the
+ * direction of the belt. Should also push in that direction.</p>
+ */
 public class Belt {
 
     private Vector2 position;
