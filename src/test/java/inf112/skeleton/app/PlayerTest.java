d2609b27a4ddc2c18e4f84a6ef917c5ea42391cd
JennStro <jennys1994@gmail.com>
2020-04-02 13:12:53 +0200

    Life is decreased when 10 dmg tokens collected test

--- src/test/java/inf112/skeleton/app/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -1,18 +1,28 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.enums.Direction;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class PlayerTest {
 
     private Player player;
+    private RallyGame game;
 
     @Before
     public void setUp() {
+        Gdx.gl = mock(GL20.class);
+        //Make a headless application in order to initialize the board. Does not show.
+        new HeadlessApplication(new EmptyApplication());
+        this.game = new RallyGame();
+        this.game.setupGame("assets/maps/Risky_Exchange.tmx");
         Vector2 pos = new Vector2(0,0);
         player = new Player(pos, 1);
     }
@@ -65,6 +75,18 @@ public class PlayerTest {
         int numberTokensBefore = player.getDamageTokens();
         player.handleDamage();
         assertEquals(numberTokensBefore+1, player.getDamageTokens());
+    }
+
+    @Test
+    public void lifeDecreasedWhenCollectedTenDamageTokensTest() {
+        Board board = game.getBoard();
+        board.addPlayer(player);
+        int livesBefore = player.getLifeTokens();
+        for (int takeDamage = 1; takeDamage <= 10; takeDamage++) {
+            player.handleDamage();
+        }
+        game.decreaseLives();
+        assertEquals(livesBefore-1, player.getLifeTokens());
     }
 
 }
