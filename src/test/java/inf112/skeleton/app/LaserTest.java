1bae42224acc98a6d19269a056009592d748b5f6
JennStro <jennys1994@gmail.com>
2020-04-24 16:27:31 +0200

    Test lasers give damage

--- src/test/java/inf112/skeleton/app/LaserTest.java
+++ src/test/java/inf112/skeleton/app/LaserTest.java
@@ -6,15 +6,16 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.board.Board;
+import inf112.skeleton.app.enums.Direction;
 import inf112.skeleton.app.objects.Laser;
 import inf112.skeleton.app.objects.player.Player;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
 public class LaserTest {
@@ -22,6 +23,7 @@ public class LaserTest {
     private RallyGame game;
     private Board board;
     private ArrayList<Laser> lasers;
+    private Player player;
 
     @Before
     public void setUp() {
@@ -32,6 +34,8 @@ public class LaserTest {
         this.game.setupGame("assets/maps/Risky Exchange.tmx");
         this.board = game.getBoard();
         this.lasers = board.lasers;
+        this.player = new Player(new Vector2(0,0), 5);
+        board.addPlayer(player);
     }
 
     /**
@@ -61,20 +65,46 @@ public class LaserTest {
 
     @Test
     public void playerBlockingLaserTest() {
-        for (int i = 0; i < 3; i++) {
-            Laser laser = lasers.get(0);
-            Vector2 blockingPosition = board.getNeighbourPosition(laser.getStartPosition(), laser.getDirection());
-            Player blockingPlayer = new Player(blockingPosition, 5);
-            board.addPlayer(blockingPlayer);
-            game.fireLasers();
-            Vector2 noLaserPosition = board.getNeighbourPosition(blockingPosition, laser.getDirection());
-            assertFalse(hasLaser(noLaserPosition));
-        }
+        Laser laser = lasers.get(0);
+        Vector2 blockingPosition = board.getNeighbourPosition(laser.getStartPosition(), laser.getDirection());
+        player.setPosition(blockingPosition);
+        game.fireLasers();
+        Vector2 noLaserPosition = board.getNeighbourPosition(blockingPosition, laser.getDirection());
+        assertFalse(hasLaser(noLaserPosition));
     }
 
     @Test
     public void playerTakesDamageWhenHitByLaserTest() {
-        // test
+        Laser laser = lasers.get(0);
+        Vector2 hitPosition = board.getNeighbourPosition(laser.getStartPosition(), laser.getDirection());
+        int damageBefore = player.getDamageTokens();
+        player.setPosition(hitPosition);
+        game.fireLasers();
+        assertEquals(damageBefore+1, player.getDamageTokens());
+    }
+
+    @Test
+    public void laserBlockedByAnotherPlayersDoesNotGiveDamageTest() {
+        Laser laser = lasers.get(0);
+        Vector2 hitPosition = board.getNeighbourPosition(laser.getStartPosition(), laser.getDirection());
+        Player blockingPlayer = new Player(new Vector2(hitPosition), 6);
+        board.addPlayer(blockingPlayer);
+        Vector2 notHitPosition = board.getNeighbourPosition(hitPosition, laser.getDirection());
+        player.setPosition(notHitPosition);
+        int damageBefore = player.getDamageTokens();
+        game.fireLasers();
+        assertEquals(damageBefore, player.getDamageTokens());
+    }
+
+    @Test
+    public void playerTakesDamageWhenOtherPlayerFiresAtPlayerTest() {
+        Vector2 neighbourPosition = board.getNeighbourPosition(player.getPosition(), Direction.EAST);
+        Player firePlayer = new Player(neighbourPosition, 6);
+        firePlayer.setDirection(Direction.WEST);
+        board.addPlayer(firePlayer);
+        int damageBefore = player.getDamageTokens();
+        game.firePlayerLaser();
+        assertEquals(damageBefore+1, player.getDamageTokens());
     }
 
 }
