9ec6f57deb7eef37ead4450da1bb6f6106eb8cbc
Magnus <magnus.tonnessen97@gmail.com>
2020-04-26 15:08:10 +0200

    Make new laser sound in RallyGame::setupGame

--- src/main/java/inf112/skeleton/app/RallyGame.java
+++ src/main/java/inf112/skeleton/app/RallyGame.java
@@ -43,7 +43,6 @@ public class RallyGame extends Game {
     public void create() {
         this.buttonSkins = new ButtonSkin();
         this.setScreen(new MenuScreen(this));
-        loadAudio();
         startMusic();
     }
 
@@ -57,6 +56,8 @@ public class RallyGame extends Game {
         this.waitForCards = new Semaphore(1);
         this.waitForCards.tryAcquire();
         this.playing = true;
+
+        this.laserSound = Gdx.audio.newSound(Gdx.files.internal("assets/Sound/LaserShot.mp3"));
 
         new Thread(this::doTurn).start();
 
@@ -89,12 +90,12 @@ public class RallyGame extends Game {
         volume = volume == 0 ? 0.5f : 0;
     }
 
-    public void loadAudio() {
+    public void loadMusic() {
         gameMusic = Gdx.audio.newMusic(Gdx.files.internal("assets/sound/menu_music.mp3"));
-        laserSound = Gdx.audio.newSound(Gdx.files.internal("assets/Sound/LaserShot.mp3"));
     }
 
     public void startMusic() {
+        loadMusic();
         gameMusic.setVolume(0.5f);
         gameMusic.setLooping(true);
         gameMusic.play();
