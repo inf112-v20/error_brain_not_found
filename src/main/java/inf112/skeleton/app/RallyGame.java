f7cc5205121533987e0b4c055dc989ad3d0fe612
Mathias <mathias.skallerud@gmail.com>
2020-05-01 17:21:59 +0200

    Add player backup to repair

--- src/main/java/inf112/skeleton/app/RallyGame.java
+++ src/main/java/inf112/skeleton/app/RallyGame.java
@@ -440,6 +440,7 @@ public class RallyGame extends Game {
             for (Vector2 repairTilePos : board.getRepairTiles()){
                 if (player.getPosition().equals(repairTilePos)){
                     player.resetDamageTokens();
+                    player.setBackup(repairTilePos, player.getDirection());
                 }
             }
         }
