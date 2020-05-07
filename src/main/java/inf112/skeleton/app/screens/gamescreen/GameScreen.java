2255dcc067e4ceead789967c15a6e17535a4be16
Magnus <magnus.tonnessen97@gmail.com>
2020-05-08 00:01:17 +0200

    Moving info label

--- src/main/java/inf112/skeleton/app/screens/gamescreen/GameScreen.java
+++ src/main/java/inf112/skeleton/app/screens/gamescreen/GameScreen.java
@@ -25,16 +25,20 @@ public class GameScreen extends StandardScreen {
         actors.initializePriorityLabels();
         actors.initializeLockedLabels();
         actors.initializePowerDownButton();
+        actors.initializeInfoLabel();
 
         float tilePx = 300f;
         float unitScale = ((float) Gdx.graphics.getHeight() / game.getBoard().getHeight()) / tilePx;
 
-        this.mapRenderer = new OrthogonalTiledMapRenderer(game.getBoard().getTiledMap(), unitScale, batch);
+        this.mapRenderer = new OrthogonalTiledMapRenderer(game.getBoard().getTiledMap(), unitScale*0.9f, batch);
         this.mapRenderer.setView(camera);
     }
 
     @Override
     public void render(float v) {
+        if (!actors.infoLabel.isAnimated()) {
+            actors.infoLabel.animate();
+        }
         actors.updateButtons();
         super.render(v);
         mapRenderer.render();
--- src/main/java/inf112/skeleton/app/screens/gamescreen/GameScreenActors.java
+++ src/main/java/inf112/skeleton/app/screens/gamescreen/GameScreenActors.java
@@ -1,6 +1,10 @@
 package inf112.skeleton.app.screens.gamescreen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -14,6 +18,7 @@ import inf112.skeleton.app.cards.ProgramCard;
 import inf112.skeleton.app.cards.Register;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class GameScreenActors {
 
@@ -45,6 +50,7 @@ public class GameScreenActors {
     private final RallyGame game;
     private final Stage stage;
     private final ProgramCardSkin cardSkin;
+    public InfoLabel infoLabel;
 
 
     public GameScreenActors(RallyGame game, Stage stage) {
@@ -386,6 +392,86 @@ public class GameScreenActors {
     public void updateLockedLabels() {
         for (int registerIndex = 0; registerIndex < 5; registerIndex++) {
             lockedLabels.get(registerIndex).setVisible(!game.mainPlayer.getRegisters().getRegister(registerIndex).isOpen());
+        }
+    }
+
+    // INFO LABEL
+
+    public void initializeInfoLabel() {
+        this.infoLabel = new InfoLabel("Dette er en info label", TimeUnit.SECONDS.toMillis(5));
+             infoLabel.setPosition(10, Gdx.graphics.getHeight() - 20);
+             infoLabel.setDeltaX(-100);
+
+             stage.addActor(infoLabel);
+    }
+
+    class InfoLabel extends Actor {
+
+        private boolean animated = false;
+        private long animationStart;
+
+        private float deltaX;
+        private float deltaY;
+
+        private final String text;
+        private final long animationDuration;
+
+        private final BitmapFont font = game.getTextSkin().getFont("title");
+
+        public InfoLabel(String text, long animationDuration) {
+            this.text = text;
+            this.animationDuration = animationDuration;
+        }
+
+        public void setDeltaX(float deltaX) {
+            this.deltaX = deltaX;
+        }
+
+        public void setDeltaY(float deltaY) {
+            this.deltaY = deltaY;
+        }
+
+        public void animate() {
+            animated = true;
+            animationStart = System.currentTimeMillis();
+        }
+
+        public boolean isAnimated() {
+            return animated;
+        }
+
+        @Override
+        public void draw(Batch batch, float parentAlpha) {
+            if (animated) {
+                // The component will auto-destruct when animation is finished.
+                if (isDisposable()) {
+                    dispose();
+                    return;
+                }
+
+                float elapsed = System.currentTimeMillis() - animationStart;
+
+                // The text will be fading.
+                font.setColor(getColor().r, getColor().g, getColor().b, parentAlpha * (1 - elapsed / animationDuration));
+
+                font.draw(batch, text, getX() + deltaX * elapsed / 1000f, getY() + deltaY * elapsed / 1000f);
+            }
+        }
+
+        /**
+         * @return true is the animation has finished.
+         */
+        private boolean isDisposable() {
+            return animationStart + animationDuration < System.currentTimeMillis();
+        }
+
+        /**
+         * Dispose the component. <b>Note that all the children components also should
+         * be disposed otherwise a memory leak will occur.</b>
+         */
+        private void dispose() {
+            font.dispose();
+            remove();
         }
     }
 }
