abe31bcce9dfd3781f4200a59b6a143f8090cbc0
JennStro <jennys1994@gmail.com>
2020-05-07 19:41:45 +0200

    star  import

--- src/test/java/inf112/skeleton/app/LaserTest.java
+++ src/test/java/inf112/skeleton/app/LaserTest.java
@@ -14,7 +14,9 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 public class LaserTest {
